aacb783688d8d63e051e6c90477f6365b90a75d7
Marc Vaccaro <marcgvaccaro@gmail.com>
2019-01-13 13:29:16 -0800

    Extend user table and add photo table

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,7 @@
-﻿namespace Friendster.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace Friendster.Models
 {
     public class User
     {
@@ -6,5 +9,23 @@
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public Gender LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Both
     }
 }
