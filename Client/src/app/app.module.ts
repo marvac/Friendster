b926c280d89b1063644242404cb9e0ef9dac8939
Marc Vaccaro <marcgvaccaro@gmail.com>
2019-01-15 22:30:55 -0800

    Add wiring for profile editing page

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -24,6 +24,9 @@ import { MemberCardComponent } from './components/members/member-card/member-car
 import { MemberDetailComponent } from './components/members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './resolvers/member-detail.resolver';
 import { MemberListResolver } from './resolvers/member-list.resolver';
+import { MemberEditComponent } from './components/members/member-edit/member-edit.component';
+import { MemberEditResolver } from './resolvers/member-edit.resolver';
+
 
 export function tokenGetter() {
   return localStorage.getItem("token");
@@ -39,7 +42,8 @@ export function tokenGetter() {
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -64,7 +68,8 @@ export function tokenGetter() {
     AuthGuard,
     UserService,
     MemberDetailResolver,
-    MemberListResolver
+    MemberListResolver,
+    MemberEditResolver
   ],
   bootstrap: [AppComponent]
 })
--- Client/src/app/routes.ts
+++ Client/src/app/routes.ts
@@ -7,6 +7,8 @@ import { AuthGuard } from './guards/auth.guard';
 import { MemberDetailComponent } from './components/members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './resolvers/member-detail.resolver';
 import { MemberListResolver } from './resolvers/member-list.resolver';
+import { MemberEditComponent } from './components/members/member-edit/member-edit.component';
+import { MemberEditResolver } from './resolvers/member-edit.resolver';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -16,7 +18,10 @@ export const appRoutes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: 'members', component: MemberListComponent, resolve: { users: MemberListResolver } },
-      { path: 'members/:id', component: MemberDetailComponent, resolve: { user: MemberDetailResolver } },
+      { path: 'members/edit', component: MemberEditComponent, resolve: { user: MemberEditResolver },
+      { path: 'members/:id', component: MemberDetailComponent,
+        resolve: { user: MemberDetailResolver }
+      },
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent }
     ]
