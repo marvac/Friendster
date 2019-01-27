3d46cf50022f96fd491af89ba10b2339c835be15
Marc Vaccaro <marcgvaccaro@gmail.com>
2019-01-26 18:00:57 -0800

    Convert register form to a reactive form

--- Client/src/app/components/register/register.component.ts
+++ Client/src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { AlertifyService } from 'src/app/services/alertify.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,18 +12,26 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl();
+    });
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alertify.success('Registered successfully')
-    }, error => {
-      this.alertify.error(error);
-    });
+    //this.authService.register(this.model).subscribe(() => {
+    //  this.alertify.success('Registered successfully')
+    //}, error => {
+    //  this.alertify.error(error);
+    //});
+
+    console.log(this.registerForm.value);
   }
 
   cancel() {
