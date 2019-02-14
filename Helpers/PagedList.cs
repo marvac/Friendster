fa5a4307d00c5c975b7e23c0930df80c22eb5dfc
Marc Vaccaro <marcgvaccaro@gmail.com>
2019-02-13 19:17:53 -0800

    Implement a paged list to return members to the client

---
+++ Helpers/PagedList.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friendster.Helpers
+{
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+
+        public int TotalCount { get; set; }
+        public int PageSize { get; set; }
+
+        public PagedList(List<T> items, int count, int currentPage, int pageSize)
+        {
+            TotalCount = count;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int currentPage, int pageSize)
+        {
+            int count = await source.CountAsync();
+            var items = await source.Skip((currentPage - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PagedList<T>(items, count, currentPage, pageSize);
+        }
+    }
+}
